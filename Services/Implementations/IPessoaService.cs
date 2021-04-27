98ad35509d146da7a44a972819b1869dd565c852
Fábio <fabiosp11@hotmail.com>
2021-04-27 18:00:58 -0300

    Desenvolvendo

---
+++ Services/Implementations/IPessoaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTeste.Services.Implementations
+{
+    interface IPessoaService
+    {
+    }
+}
