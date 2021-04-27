12f3680020ab96f3d404ef50244a7ba6669b9b00
Fábio <fabiosp11@hotmail.com>
2021-04-27 17:53:59 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTeste.Model
+{
+    public class Pessoa
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Endereço { get; set; }
+        public string Genero { get; set; }
+   
+   
+
+    }
+}
